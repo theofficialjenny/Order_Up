c2542b74d765b8fb42d4fca7ae28e2b67b910b46
theofficialjenny <jenniferbenjamin235@gmail.com>
2025-10-19 22:25:21 +1300

    Home and Search interfaces.

--- app/src/main/java/com/example/orderup/DineIn.java
+++ app/src/main/java/com/example/orderup/DineIn.java
@@ -1,8 +1,6 @@
 package com.example.orderup;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,25 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class HomeActivity extends AppCompatActivity {
+public class DineIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_dine_in);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void openSearchActivity(View view) {
-        startActivity(new Intent(this, Search.class));
-    }
-
-    public void openHomeActivity(View view) {
-        startActivity(new Intent(this, HomeActivity.class));
     }
 }
--- app/src/main/java/com/example/orderup/Search.java
+++ app/src/main/java/com/example/orderup/Search.java
@@ -10,13 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class HomeActivity extends AppCompatActivity {
+public class Search extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_search);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -24,11 +24,5 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
-    public void openSearchActivity(View view) {
-        startActivity(new Intent(this, Search.class));
-    }
 
-    public void openHomeActivity(View view) {
-        startActivity(new Intent(this, HomeActivity.class));
-    }
 }
